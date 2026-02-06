85c57f3ec9609d9ac6d17b597f95089bcc51d5b9
kelly <kiroegbu@gmail.com>
2026-02-06 15:06:20 +0100

    Added

--- prodev-mobile-app-0x04/styles/_join.ts
+++ prodev-mobile-app-0x04/styles/_join.ts
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginTop: 22
+    marginTop: 21
   },
   titleTextGroup: {
     marginTop: 20
